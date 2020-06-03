5eadd7ce01969696fced0fa0dcf4ef6a1a31b58d
Matheus Alexandre <mathe.system@gmail.com>
2020-06-02 22:13:48 -0300

    refactor: move sut creation to a factory method

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -5,16 +5,20 @@ class EmailValidator {
   }
 }
 
+const makeSut = () => {
+  return new EmailValidator()
+}
+
 describe('Email Validator', () => {
   it('should return true if validator returns true', () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('valid_email@mail.com')
     expect(isEmailValid).toBe(true)
   })
 
   it('should return false if validator returns false', () => {
     validator.isEmailValid = false
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('invalid_email@mail.com')
     expect(isEmailValid).toBe(false)
   })
