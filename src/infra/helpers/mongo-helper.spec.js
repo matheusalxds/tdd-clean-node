ed50497cfb051113ab8eb627bbf5c8eb5bd7a995
Matheus Alexandre <mathe.system@gmail.com>
2020-06-06 08:44:11 -0300

    test: ensure MongoHelper reconnect when getDb() is invoked and client is disconnected

---
+++ src/infra/helpers/mongo-helper.spec.js
@@ -0,0 +1,35 @@
+const sut = require('./mongo-helper')
+
+describe('Mongo Helper', () => {
+  beforeAll(async () => {
+    await sut.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await sut.disconnect()
+  })
+
+  test('should reconnect if mongodb is down', async () => {
+    expect(sut.db).toBeTruthy()
+
+    await sut.disconnect()
+    expect(sut.db).toBeFalsy()
+
+    await sut.getDb()
+    expect(sut.db).toBeTruthy()
+  })
+})
+
+// describe('Mongo Helper', () => {
+//   test('should reconnect when getDb is invoked and client is disconnected', async () => {
+//     const sut = MongoHelper
+//     await sut.connect(process.env.MONGO_URL)
+//     expect(sut.db).toBeTruthy()
+
+//     await sut.disconnect()
+//     expect(sut.db).toBeFalsy()
+
+//     await sut.getDb()
+//     expect(sut.db).toBeTruthy()
+//   })
+// })
