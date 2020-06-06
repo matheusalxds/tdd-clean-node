e957640cd4f35c8739471bfe8d0ec2e001f5d7bf
Matheus Alexandre <mathe.system@gmail.com>
2020-06-06 08:58:55 -0300

    test: ensure UpdateAccessTokenRepository updates user with the given accessToken

--- src/infra/helpers/mongo-helper.spec.js
+++ src/infra/helpers/mongo-helper.spec.js
@@ -19,17 +19,3 @@ describe('Mongo Helper', () => {
     expect(sut.db).toBeTruthy()
   })
 })
-
-// describe('Mongo Helper', () => {
-//   test('should reconnect when getDb is invoked and client is disconnected', async () => {
-//     const sut = MongoHelper
-//     await sut.connect(process.env.MONGO_URL)
-//     expect(sut.db).toBeTruthy()
-
-//     await sut.disconnect()
-//     expect(sut.db).toBeFalsy()
-
-//     await sut.getDb()
-//     expect(sut.db).toBeTruthy()
-//   })
-// })
