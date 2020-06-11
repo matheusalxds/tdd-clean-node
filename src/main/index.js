c207cc2b4799a140c48141d39e1f7473d62ef1a4
Matheus Alexandre <mathe.system@gmail.com>
2020-06-11 00:38:05 -0300

    fix: call function correctly in routes config file

--- src/main/index.js
+++ src/main/index.js
@@ -1,9 +1,9 @@
-const app = require('./config/app')
 const MongoHelper = require('../infra/helpers/mongo-helper')
 const env = require('./config/env')
 
 MongoHelper.connect(env.mongoUrl)
   .then(() => {
+    const app = require('./config/app')
     app.listen(5858, () => console.log('Running!'))
   })
   .catch(console.error)
